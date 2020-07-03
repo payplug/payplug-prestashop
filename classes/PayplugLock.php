3c327a5edcbf098913776ae1848ea1fb1e77e8af
hadrien de laforce <hdelaforce@payplug.com>
2020-07-03 16:06:52 +0200

    update PayPlugLock::check() method

--- classes/PayplugLock.php
+++ classes/PayplugLock.php
@@ -107,17 +107,20 @@ class PayplugLock extends ObjectModel
      * @param  int $loop_time
      * @return void
      */
-    public static function check($id_cart, $loop_time = 1)
+    public static function check($id_cart, $loop_time = 1, $process = 'none')
     {
-        //definir le delai
+        // Set delay
         $delay = new DateInterval('PT10S');
+        $lifetime = new DateInterval('PT20S');
 
-        //test s'il y a un lock
+        // Check if lock exists
         $lock_exists = self::existsLockG2($id_cart);
         if ($lock_exists) {
-            //definir la date de fin du lock
+            // Then define the expiration
             $last_update = new DateTime($lock_exists['date_upd']);
             $last_check = $last_update->add($delay);
+            $creation_date = new DateTime($lock_exists['date_add']);
+            $end_of_life = $creation_date->add($lifetime);
             $time = new DateTime('now');
 
             while ((self::existsLockG2($id_cart) !== false) && ($time < $last_check)) {
@@ -125,6 +128,16 @@ class PayplugLock extends ObjectModel
                     usleep($loop_time * 1000000);
                 } else {
                     self::usleep($loop_time * 1000);
+                }
+
+
+                // If lock take too much time, end the process
+                if ($time > $end_of_life) {
+                    if ($process == 'validation') {
+                        self::deleteLockG2($id_cart);
+                    } else {
+                        return 'stop ipn';
+                    }
                 }
 
                 $time = new DateTime('now');
