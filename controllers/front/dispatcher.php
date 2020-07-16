5244277fdaffdd29805755fa3bf13e415a49c292
hadrien de laforce <hdelaforce@payplug.com>
2020-07-16 10:19:05 +0200

    fix affectation & psr

--- controllers/front/dispatcher.php
+++ controllers/front/dispatcher.php
@@ -30,13 +30,15 @@ class PayplugDispatcherModuleFrontController extends ModuleFrontController
      */
     public function postProcess()
     {
-        if($method = Tools::getValue('method')) {
+        if ($method = Tools::getValue('method')) {
             $payplug = new Payplug();
             $id_cart = (int)Tools::getValue('id_cart');
             $id_card = Tools::getValue('pc');
             $is_deferred = (bool)Tools::getValue('def');
-            $is_one_click = (bool)($method == 'one_click');
-            $is_installment = (bool)($method == 'installment');
+            $is_one_click = (bool)($method === 'one_click');
+            $is_installment = (bool)($method === 'installment');
+            $oney_type = Tools::getValue('oney_type');
+            $is_oney = (bool)($method === 'oney' && $oney_type);
 
 
             $cart = new Cart($id_cart);
@@ -47,15 +49,14 @@ class PayplugDispatcherModuleFrontController extends ModuleFrontController
             $options = $payplug->getAvailableOptions($cart);
 
             $error_url = 'index.php?controller=order&step=3&error=1';
-            if($options['oney'] && $method = 'oney' && $oney_type = Tools::getValue('oney_type')) {
+            if ($options['oney'] && $is_oney) {
                 $payment = $payplug->preparePayment(['is_oney' => $oney_type]);
-                if(!$payment['result']) {
+                if (!$payment['result']) {
                     Tools::redirect($error_url);
                 } else {
                     Tools::redirect($payment['return_url']);
                 }
-            }
-            // if the payment is redirect and not a one click payment, prepare the payment and redirect
+            } // if the payment is redirect and not a one click payment, prepare the payment and redirect
             elseif (!$options['embedded'] && !$is_one_click) {
                 $payment_options = [
                     'id_card' => $id_card,
@@ -63,19 +64,18 @@ class PayplugDispatcherModuleFrontController extends ModuleFrontController
                     'is_deferred' => $is_deferred,
                 ];
                 $payment = $payplug->preparePayment($payment_options);
-                if(!$payment['result']) {
+                if (!$payment['result']) {
                     $payplug->setPaymentErrorsCookie([$payplug->l('The transaction was not completed and your card was not charged.')]);
                     Tools::redirect($error_url);
                 } else {
                     Tools::redirect($payment['return_url']);
                 }
-            }
-            // else reload the page with lightbox arg
+            } // else reload the page with lightbox arg
             else {
                 $return_url = 'index.php?controller=order&step=3&lightbox=1'
                     . ($is_installment ? '&inst=1' : '')
                     . ($is_one_click ? '&pc=' . $id_card : '')
-                    . '&def='.(int)Tools::getValue('def');
+                    . '&def=' . (int)Tools::getValue('def');
                 Tools::redirect($return_url);
             }
         }
