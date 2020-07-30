fae12305d5037f565717a99c128b0abf883eaec6
Aurélie Lopez <alopez@payplug.com>
2020-07-30 12:24:12 +0000

    check "quantity_wanted" value as well as "qty"

--- payplug.php
+++ payplug.php
@@ -4080,7 +4080,8 @@ class Payplug extends PaymentModule
             $id_product = (int)Tools::getValue('id_product');
             $group = Tools::getValue('group');
             $id_product_attribute = $group ? (int)Product::getIdProductAttributeByIdAttributes($id_product, $group) : 0;
-            $quantity = (int)Tools::getValue('qty', 1);
+            // Some integration will not use qty data but quantity_wanted
+            $quantity = (int)Tools::getValue('qty', (int)Tools::getValue('quantity_wanted', 1));
 
             $product_price = Product::getPriceStatic((int)$id_product, $use_taxes, $id_product_attribute, 6, null,
                 false, true, $quantity);
