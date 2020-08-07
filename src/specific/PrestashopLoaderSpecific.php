a5e3fe7df9ef76869551a6da6a785c5af3ec4a4f
Cedric Touma <ctouma@payplug.com>
2020-08-07 16:34:35 +0200

    specific

--- src/specific/PrestashopLoaderSpecific.php
+++ src/specific/PrestashopLoaderSpecific.php
@@ -1,6 +1,2 @@
 <?php
-$specificPresta = _PS_MODULE_DIR_ . 'payplug/src/specific/PrestashopSpecific'._PS_VERSION_[0]._PS_VERSION_[2].'.php';
-if (is_file($specificPresta))
-{
-    require_once($specificPresta);
-}
+include_once(_PS_MODULE_DIR_ . 'payplug/src/specific/PrestashopSpecific'._PS_VERSION_[0]._PS_VERSION_[2].'.php');
