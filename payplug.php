bdb8a3da431a7e673d63aa5d65958370e6a2f4af
Cedric Touma <ctouma@payplug.com>
2020-08-07 17:59:28 +0200

    add missing functions

--- payplug.php
+++ payplug.php
@@ -6724,6 +6724,151 @@ class Payplug extends PaymentModule
     }
 
     /**
+     * @param $cart
+     */
+    public function assignPaymentOptions($cart)
+    {
+        $one_click = $this->getConfiguration('PAYPLUG_ONE_CLICK');
+        $installment = $this->getConfiguration('PAYPLUG_INST');
+        $installment_mode = $this->getConfiguration('PAYPLUG_INST_MODE');
+        $installment_min_amount = $this->getConfiguration('PAYPLUG_INST_MIN_AMOUNT');
+
+        if (!$this->checkCurrency($cart) ||
+            !$this->checkAmount($cart)) {
+            return false;
+        }
+
+        $path_ssl = Tools::getShopDomainSsl(true, true) . __PS_BASE_URI__ . 'modules/' . $this->name . '/';
+
+        $payplug_card = new PayPlugCard();
+        $payplug_cards = $payplug_card->getByCustomer($cart->id_customer, true);
+
+        $use_taxes = $this->getConfiguration('PS_TAX');
+        $base_total_tax_inc = $cart->getOrderTotal(true);
+        $base_total_tax_exc = $cart->getOrderTotal(false);
+
+        if ($base_total_tax_inc < $installment_min_amount) {
+            $installment = 0;
+        }
+
+        if ($use_taxes) {
+            $price2display = $base_total_tax_inc;
+        } else {
+            $price2display = $base_total_tax_exc;
+        }
+
+        $this->smarty->assign(array(
+            'this_path' => $this->_path,
+            'this_path_ssl' => $path_ssl,
+            'iso_lang' => $this->context->language->iso_code,
+            'price2display' => $price2display,
+        ));
+
+        $front_ajax_url = PayplugBackward::getModuleLink($this->name, 'ajax', array(), true);
+
+        $this->smarty->assign(array(
+            'front_ajax_url' => $front_ajax_url,
+            'api_url' => $this->api_url
+        ));
+
+        if (!empty($payplug_cards) && $one_click == 1) {
+            $this->smarty->assign(array(
+                'payplug_cards' => $payplug_cards,
+                'payplug_one_click' => 1,
+            ));
+        }
+
+        if (version_compare(_PS_VERSION_, '1.5', '<')) {
+            $payment_url = 'order.php?step=3';
+        } else {
+            $payment_url = 'index.php?controller=order&step=3';
+        }
+
+        $payment_controller_url = PayplugBackward::getModuleLink($this->name, 'payment', array(), true);
+        $installment_controller_url = PayplugBackward::getModuleLink($this->name, 'payment', array('i' => 1), true);
+        $current_lang = explode('-', $this->context->language->language_code);
+        $current_lang = $current_lang[0];
+        if (in_array($current_lang, array('it', 'en'))) {
+            $img_lang = $current_lang;
+        } else {
+            $img_lang = 'default';
+        }
+
+        $this->smarty->assign(array(
+            'spinner_url' => PayplugBackward::getHttpHost(true)
+                . __PS_BASE_URI__ . 'modules/payplug/views/img/admin/spinner.gif',
+            'payment_url' => $payment_url,
+            'payment_controller_url' => $payment_controller_url,
+            'installment_controller_url' => $installment_controller_url,
+            'img_lang' => $img_lang,
+            'payplug_installment' => $installment,
+            'installment_mode' => $installment_mode,
+        ));
+    }
+
+    /**
+    * Assign Oney var
+    *
+    * @param $cart Cart
+    * @return bool
+    */
+    private function assignOneyPaymentOptions($cart)
+    {
+        if (!$this->getConfiguration('PAYPLUG_ONEY')) {
+            return false;
+        }
+
+        // check if at least one carrier is available for this cart
+        // get the available carrier
+        $package_list = $cart->getPackageList();
+        $carrier_ids = array();
+        foreach ($package_list as $address) {
+            foreach ($address as $package) {
+                $carrier_ids = array_merge($carrier_ids, $package['carrier_list']);
+            }
+        }
+
+        // only if we have carrier need for this cart
+        // check the carrier type of each available carrier
+        if ($carrier_ids) {
+            $has_valid_carrier = false;
+            foreach ($carrier_ids as $carrier_id) {
+                if ($has_valid_carrier) {
+                    continue;
+                }
+
+                $pc = new PayPlugCarrier();
+                $pc = $pc->getByIdCarrier($carrier_id);
+                if ($pc->delivery_type) {
+                    $has_valid_carrier = true;
+                }
+            }
+
+            // if no carrier available for Oney, return false
+            if (!$has_valid_carrier) {
+                return false;
+            }
+        }
+
+        if (Validate::isLoadedObject($cart) && $cart->id_address_invoice && $cart->id_address_delivery) {
+            $is_elligible = $this->isOneyElligible($cart);
+        } else {
+            $id_currency = $this->context->currency->id;
+            $amount = $cart->getOrderTotal(true, Cart::BOTH);
+            $is_elligible = $this->isValidOneyAmount($amount, $id_currency);
+        }
+
+        $this->smarty->assign(array(
+            'payplug_module_dir' => _PS_MODULE_DIR_,
+            'payplug_oney' => true,
+            'payplug_oney_required_field' => $this->displayOneyRequiredFields(),
+            'payplug_oney_allowed' => $is_elligible['result'],
+            'payplug_oney_error' => $is_elligible['error'],
+            'payplug_oney_loading_msg' => $this->l('Loading')
+        ));
+    }
+
+    /**
      * Install the required hooks
      * @return bool
      */
