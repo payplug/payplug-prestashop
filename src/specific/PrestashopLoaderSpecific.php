e9f1f40f6514ea2fe4a7f011dc3894c6c9cec178
Cedric Touma <ctouma@payplug.com>
2020-08-21 14:36:32 +0200

    Fix Loader 1.7 specific file

--- src/specific/PrestashopLoaderSpecific.php
+++ src/specific/PrestashopLoaderSpecific.php
@@ -1,2 +1,7 @@
 <?php
-include_once(_PS_MODULE_DIR_ . 'payplug/src/specific/PrestashopSpecific'._PS_VERSION_[0]._PS_VERSION_[2].'.php');
+$PSfile = _PS_MODULE_DIR_ . 'payplug/src/specific/PrestashopSpecific'._PS_VERSION_[0]._PS_VERSION_[2].'.php';
+
+// PS 1.7 n'aime pas 'include_once' si le fichier n'existe pas : Erreur pdt install du module
+if (is_file($PSfile)) {
+    require_once ($PSfile);
+}
