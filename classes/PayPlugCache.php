3307d34898a5b0fcd1c45e1b8852052e659b6b56
Cedric Touma <ctouma@payplug.com>
2020-10-01 16:23:01 +0200

    Replace use

--- classes/PayPlugCache.php
+++ classes/PayPlugCache.php
@@ -21,11 +21,11 @@
  *  International Registered Trademark & Property of PayPlug SAS
  */
 
-use PayPlug\classes\PayPlugLogger;
-
 if (!defined('_PS_VERSION_')) {
     exit;
 }
+
+use PayPlug\classes\PayPlugLogger;
 
 /**
  * @description The PayPlug Cache class is used to store and retrieve every Oney Simulation
