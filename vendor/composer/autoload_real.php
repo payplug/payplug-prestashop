38c3b6cc599ce5dea29e5ac397748f99f6ac3ad2
hadrien de laforce <hdelaforce@payplug.com>
2020-11-12 14:39:39 +0100

    update composer

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,9 +13,6 @@ class ComposerAutoloaderInit6fe1e2be2c1f64472e8524add3e400eb
         }
     }
 
-    /**
-     * @return \Composer\Autoload\ClassLoader
-     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
