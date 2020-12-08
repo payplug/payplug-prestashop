fe3705f59c753787ace6285cbfb3d70d98bea045
Cedric Touma <ctouma@payplug.com>
2020-12-08 15:15:45 +0100

    remove context

--- src/specific/PrestashopSpecific17.php
+++ src/specific/PrestashopSpecific17.php
@@ -83,7 +83,7 @@ class PrestashopSpecific17
 
     public function hookActionAdminControllerSetMedia()
     {
-        $this->payplug->context->controller->addCSS(__PS_BASE_URI__ . 'modules/payplug/views/css/admin_order.css');
-        $this->payplug->context->controller->addJS(__PS_BASE_URI__ . 'modules/payplug/views/js/admin_order.js');
+        $this->payplug->addCSS(__PS_BASE_URI__ . 'modules/payplug/views/css/admin_order.css');
+        $this->payplug->addJS(__PS_BASE_URI__ . 'modules/payplug/views/js/admin_order.js');
     }
 }
