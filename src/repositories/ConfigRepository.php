103d36ee8fbda0609a72f5e59923b1de33649d04
Cedric Touma <ctouma@payplug.com>
2021-01-14 08:07:22 +0100

    add missing config repo

--- src/repositories/ConfigRepository.php
+++ src/repositories/ConfigRepository.php
@@ -1151,7 +1151,7 @@ class ConfigRepository extends Repository
                         up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i',
             $useragent
         ) || preg_match(
-                '/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|
+            '/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|
             an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|
                 br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|
                 dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|
@@ -1168,8 +1168,8 @@ class ConfigRepository extends Repository
                 tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|
                 vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|
                 wmlb|wonu|x700|yas\-|your|zeto|zte\-/i',
-                Tools::substr($useragent, 0, 4)
-            )) {
+            Tools::substr($useragent, 0, 4)
+        )) {
             return true;
         }
         return false;
