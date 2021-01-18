857ce053a62d600190c4bb3cfe1c3ccb9e3d772b
hadrien de laforce <hdelaforce@payplug.com>
2021-01-18 15:26:56 +0100

    add mock for tu

---
+++ tests/mock/CustomerMock.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class CustomerMock
+{
+    public static function getCustomer()
+    {
+        $customer = new \stdClass();
+        $customer->id = 1;
+        $customer->lastname = "Lorem";
+        $customer->firstname = "Ipsum";
+        $customer->email = "customer@payplug.com";
+        $customer->date_add = "2020-12-21 12:03:52";
+        $customer->date_upd = "2021-01-11 11:24:31";
+        return $customer;
+    }
+}
