f2935b1a9388df0ee55bc1c04e276f53b14be3e2
hadrien de laforce <hdelaforce@payplug.com>
2021-01-21 16:18:53 +0100

    add tu: getTable

--- tests/entities/CardEntity/GetTableCardTest.php
+++ tests/entities/CardEntity/GetTableCardTest.php
@@ -31,45 +31,28 @@ use PHPUnit\Framework\TestCase;
  * @group card
  * @group card_entity
  */
-final class GetMetadataTest extends TestCase
+final class GetTableTest extends TestCase
 {
     protected $card;
-    protected $metadata;
 
     protected function setUp()
     {
         $this->card = new CardEntity();
-        $metadata = [
-            'key1' => 'value1',
-            'key2' => 'value2',
-            'key3' => 3,
-        ];
-        $this->metadata = json_encode($metadata);
-        $this->card->setMetadata(
-            $this->metadata
-        );
+        $this->card->setTable('table_name');
     }
 
-    public function testReturnMetadata()
+    public function testReturnTable()
     {
         $this->assertSame(
-            $this->metadata,
-            $this->card->getMetadata()
+            'table_name',
+            $this->card->getTable()
         );
     }
 
-    public function testMetadataIsAnInt()
+    public function testTableIsAnInt()
     {
         $this->assertTrue(
-            is_string($this->card->getMetadata())
-        );
-    }
-
-    public function testMetadataIsAnJsonEncode()
-    {
-        $metadata = json_decode($this->card->getMetadata(), true);
-        $this->assertTrue(
-            is_array($metadata)
+            is_string($this->card->getTable())
         );
     }
 }
