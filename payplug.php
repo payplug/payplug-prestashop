d78f494b67067075d6b3153f136b7c8ca27d93e5
Aurélie Lopez <alopez@payplug.com>
2021-01-21 13:37:27 +0000

    Revert "Merge branch 'release-3.0.7' into 'master'"
    This reverts merge request !452

--- payplug.php
+++ payplug.php
@@ -6580,7 +6580,7 @@ class Payplug extends PaymentModule
         $this->need_instance = true;
         $this->ps_versions_compliancy = array('min' => '1.7', 'max' => '1.8');
         $this->tab = 'payments_gateways';
-        $this->version = '3.0.7';
+        $this->version = '2.31.1';
         $this->api_version = '2019-08-06';
     }
 
