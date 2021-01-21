f0027db383066419adb227de96ef3148e287873a
hadrien de laforce <hdelaforce@payplug.com>
2021-01-21 17:06:00 +0100

    add tu: setCountry

---
+++ tests/entities/CardEntity/SetCountryCardTest.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * 2013 - 2021 PayPlug SAS
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0).
+ * It is available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/osl-3.0.php
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to contact@payplug.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PayPlug module to newer
+ * versions in the future.
+ *
+ * @author    PayPlug SAS
+ * @copyright 2013 - 2021 PayPlug SAS
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *  International Registered Trademark & Property of PayPlug SAS
+ */
+
+declare(strict_types=1);
+
+use PayPlug\src\entities\CardEntity;
+use PayPlug\src\exceptions\BadParameterException;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group entity
+ * @group card
+ * @group card_entity
+ */
+final class SetCountryCardTest extends TestCase
+{
+    protected $card;
+
+    protected function setUp()
+    {
+        $this->card = new CardEntity();
+        $this->card->setCountry('country_name');
+    }
+
+    public function testUpdateCountry()
+    {
+        $this->card->setCountry('new_country_name');
+        $this->assertSame(
+            'new_country_name',
+            $this->card->getCountry()
+        );
+    }
+
+    public function testReturnCardEntity()
+    {
+        $this->assertInstanceOf(
+            CardEntity::class,
+            $this->card->setCountry('country_name')
+        );
+    }
+}
