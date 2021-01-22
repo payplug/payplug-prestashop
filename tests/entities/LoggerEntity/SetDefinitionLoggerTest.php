80e6c4910fb3b764ca040c1f40c5b6d7722d6450
hadrien de laforce <hdelaforce@payplug.com>
2021-01-22 14:47:05 +0100

    add tu: SetDefinitionLoggerTest

---
+++ tests/entities/LoggerEntity/SetDefinitionLoggerTest.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * 2013 - 2021 PayPlug SAS
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0).
+ * It is available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/osl-3.0.php
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to contact@payplug.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PayPlug module to newer
+ * versions in the future.
+ *
+ * @author    PayPlug SAS
+ * @copyright 2013 - 2021 PayPlug SAS
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *  International Registered Trademark & Property of PayPlug SAS
+ */
+
+declare(strict_types=1);
+
+use PayPlug\src\entities\LoggerEntity;
+use PayPlug\src\exceptions\BadParameterException;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group entity
+ * @group logger
+ * @group logger_entity
+ */
+final class SetDefinitionLoggerTest extends TestCase
+{
+    protected $logger;
+    protected $definition;
+    protected $definition_alt;
+
+    protected function setUp()
+    {
+        $this->logger = new LoggerEntity();
+        $this->definition = [
+            'key1' => 'value1',
+            'key2' => 'value2',
+            'key3' => 3,
+        ];
+        $this->definition_alt = [
+            'keyA' => 'valueA',
+            'keyB' => 'valueB',
+            'keyC' => 8,
+        ];
+        $this->logger->setDefinition($this->definition);
+    }
+
+    public function testUpdateDefinition()
+    {
+        $this->logger->setDefinition($this->definition_alt);
+        $this->assertSame(
+            $this->definition_alt,
+            $this->logger->getDefinition()
+        );
+    }
+
+    public function testReturnLoggerEntity()
+    {
+        $this->assertInstanceOf(
+            LoggerEntity::class,
+            $this->logger->setDefinition($this->definition_alt)
+        );
+    }
+
+    /**
+     * @group entity_exception
+     * @group logger_exception
+     * @group logger_entity_exception
+     * @group exception
+     */
+    public function testThrowExceptionWhenNotAnArray()
+    {
+        $this->expectException(BadParameterException::class);
+        $this->logger->setDefinition(42);
+    }
+}
