07eaa130347d306906ab62027159ec7425d6fbbe
hadrien de laforce <hdelaforce@payplug.com>
2021-01-22 14:45:19 +0100

    add tu: GetDefinitionLoggerTest

---
+++ tests/entities/LoggerEntity/GetDefinitionLoggerTest.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * 2013 - 2021 PayPlug SAS
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0).
+ * It is available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/osl-3.0.php
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to contact@payplug.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PayPlug module to newer
+ * versions in the future.
+ *
+ * @author    PayPlug SAS
+ * @copyright 2013 - 2021 PayPlug SAS
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *  International Registered Trademark & Property of PayPlug SAS
+ */
+
+declare(strict_types=1);
+
+use PayPlug\src\entities\LoggerEntity;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group entity
+ * @group logger
+ * @group logger_entity
+ */
+final class GetDefinitionLoggerTest extends TestCase
+{
+    protected $logger;
+    protected $definition;
+
+    protected function setUp()
+    {
+        $this->logger = new LoggerEntity();
+        $this->definition = [
+            'key1' => 'value1',
+            'key2' => 'value2',
+            'key3' => 3,
+        ];
+        $this->logger->setDefinition($this->definition);
+    }
+
+    public function testReturnDefinition()
+    {
+        $this->assertSame(
+            $this->definition,
+            $this->logger->getDefinition()
+        );
+    }
+
+    public function testDefinitionIsAnArray()
+    {
+        $this->assertTrue(
+            is_array($this->logger->getDefinition())
+        );
+    }
+}
