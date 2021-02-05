e3d99bb035cbb0b70112a821ada51ebd8da1ed72
Aurelie Lopez <alopez@payplug.com>
2021-02-05 17:50:27 +0100

    Add revision script on gitlab-ci + ignore script un build package

---
+++ assets_revision.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+versionning_assets() {
+  extension=$1
+  version=$2
+  for file in views/$extension/*.$extension;do
+      if [[ `basename ${file}` == *"-v3"* ]]; then
+        echo "File" `basename ${file}` "already versioned: cannot perform script"
+        exit 1
+      fi
+      echo `basename ${file}`
+      mv ${file} ${file%%.*}'-v'$version'.'$extension
+      git grep -rl `basename ${file}` .
+      git grep -rl `basename ${file}` . | xargs sed -i -e 's/'`basename ${file}`'/'`basename ${file%%.*}`'-v'$version'.'$extension'/g';
+  done
+}
+
+echo "Looking for tag in payplug.php..."
+tag=`grep '$this->version =' payplug.php | sed -n "s/.*= '//p" | sed -n "s/';//p"`
+
+echo "Starting versioning JS file with" $tag "tag..."
+versionning_assets 'js' $tag
+echo "***** DONE *****"
+
+echo "Starting versioning CSS file with '" $tag "' tag..."
+versionning_assets 'css' $tag
+echo "***** DONE *****"
+
+echo " FINISH !!!"
+ls -l views/js/*.js
+ls -l views/css/*.css
