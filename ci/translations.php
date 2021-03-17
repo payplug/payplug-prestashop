d0c15ff714f43380d65fb7ee6679455326830fc2
hadrien de laforce <hdelaforce@payplug.com>
2021-03-17 15:24:51 +0100

    upd gitlab ci

--- ci/translations.php
+++ ci/translations.php
@@ -1,32 +1,30 @@
 <?php
-$messages = [
-        'coucou'
-];
-//require_once(dirname(__FILE__) . '/../vendor/autoload.php');
-//
-//$repo = new PayPlug\src\repositories\TranslationsRepository('');
-//$translations = $repo->getTranslations();
-//$missing_translations = [];
-//$available_languages = ['fr','en','gb','it'];
-//
-//foreach ($translations as $key => $trans) {
-//    foreach ($available_languages as $lang) {
-//        if (!$trans[$lang]) {
-//            $missing_translations[$lang][$key] = $trans['default'];
-//        }
-//    }
-//}
-//
-//if (!empty($missing_translations)) {
-//    $messages[] = '/!\ /!\ /!\ Some translations are missing /!\ /!\ /!\ ';
-//    foreach($missing_translations as $lang => $translations) {
-//        $messages[] = 'There is ' . count($translations) . ' translations missing for the language "' . $lang . '":';
-//        foreach($translations as $key => $trans) {
-//            $trans = preg_replace("/\s+/", " ", $trans);
-//            $messages[] = $key . ' => ' . $trans;
-//        }
-//    }
-//}
+require_once(dirname(__FILE__) . '/../vendor/autoload.php');
+
+$repo = new PayPlug\src\repositories\TranslationsRepository('');
+$translations = $repo->getTranslations();
+$missing_translations = [];
+$available_languages = ['fr','en','gb','it'];
+$messages = [];
+
+foreach ($translations as $key => $trans) {
+    foreach ($available_languages as $lang) {
+        if (!$trans[$lang]) {
+            $missing_translations[$lang][$key] = $trans['default'];
+        }
+    }
+}
+
+if (!empty($missing_translations)) {
+    $messages[] = '/!\ /!\ /!\ Some translations are missing /!\ /!\ /!\ ';
+    foreach($missing_translations as $lang => $translations) {
+        $messages[] = 'There is ' . count($translations) . ' translations missing for the language "' . $lang . '":';
+        foreach($translations as $key => $trans) {
+            $trans = preg_replace("/\s+/", " ", $trans);
+            $messages[] = $key . ' => ' . $trans;
+        }
+    }
+}
 
 if (!empty($messages)) {
     foreach ($messages as $message) {
