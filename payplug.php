aa1b87d1528cc8818a13a777b594b5efed041569
hadrien de laforce <hdelaforce@payplug.com>
2021-04-16 13:14:46 +0200

    update version module

--- payplug.php
+++ payplug.php
@@ -266,7 +266,7 @@ class Payplug extends PaymentModule
         $this->need_instance = true;
         $this->ps_versions_compliancy = ['min' => '1.6', 'max' => '1.8'];
         $this->tab = 'payments_gateways';
-        $this->version = '3.2.0';
+        $this->version = '3.1.4';
         $this->oneyLogoUrl = '';
 
         $this->initializeAccessors();
