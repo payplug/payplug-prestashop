a6f30aa7a61683fe73d081bd792c83bef04bff9a
hadrien de laforce <hdelaforce@payplug.com>
2021-04-28 12:13:44 +0200

    fix: improve payplug card flush

--- payplug.php
+++ payplug.php
@@ -3417,7 +3417,7 @@ class Payplug extends PaymentModule
             $inst_status = $installment->is_active ?
                 $this->l('ongoing') :
                 (
-                    $installment->is_fully_paid ?
+                $installment->is_fully_paid ?
                     $this->l('paid') :
                     $this->l('suspended')
                 );
@@ -4373,13 +4373,13 @@ class Payplug extends PaymentModule
         $useragent = $_SERVER['HTTP_USER_AGENT'];
 
         if (preg_match(
-            '/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|
+                '/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|
             iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|
             palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|
             up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i',
-            $useragent
-        ) || preg_match(
-            '/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|
+                $useragent
+            ) || preg_match(
+                '/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|
             an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|
             br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|
             dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|
@@ -4396,8 +4396,8 @@ class Payplug extends PaymentModule
             tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|
             vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|
             wmlb|wonu|x700|yas\-|your|zeto|zte\-/i',
-            Tools::substr($useragent, 0, 4)
-        )) {
+                Tools::substr($useragent, 0, 4)
+            )) {
             return true;
         }
         return false;
@@ -6026,31 +6026,24 @@ class Payplug extends PaymentModule
      */
     private function uninstallCards()
     {
-        try {
-            $exists = Db::getInstance()->executeS('SHOW TABLES LIKE "' . _DB_PREFIX_ . 'payplug_card"');
-        } catch (Exception $e) {
-            // todo: add error log - payplug_card does not seem to exist
-            return true;
-        }
+        if ((new \PayPlug\src\repositories\SQLtableRepository)->checkExistingTable('payplug_card', 1)) {
+            $cards = $this->query
+                ->select()
+                ->fields('*')
+                ->from(_DB_PREFIX_ . 'payplug_card')
+                ->build();
 
-        if (!$exists) {
-            return true;
-        }
-
-        $req_all_cards = new DbQuery();
-        $req_all_cards->select('pc.*');
-        $req_all_cards->from('payplug_card', 'pc');
-        $res_all_cards = Db::getInstance()->executeS($req_all_cards);
-
-        if (!empty($res_all_cards)) {
-            foreach ($res_all_cards as $card) {
-                $id_customer = $card['id_customer'];
-                $id_payplug_card = $card['id_payplug_card'];
-                if (!$this->card->deleteCard($id_customer, $id_payplug_card)) {
-                    return false;
+            if ($cards) {
+                foreach ($cards as $card) {
+                    $id_customer = $card['id_customer'];
+                    $id_payplug_card = $card['id_payplug_card'];
+                    if (!$this->card->deleteCard($id_customer, $id_payplug_card)) {
+                        return false;
+                    }
                 }
             }
         }
+
         return true;
     }
 
