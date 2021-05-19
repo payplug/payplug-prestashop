3ae0ffda3a55944b76bed26c207a372e8c2fe06d
hadrien de laforce <hdelaforce@payplug.com>
2021-05-19 16:55:08 +0200

    update translation script to generate csv file

--- ci/translations.php
+++ ci/translations.php
@@ -4,36 +4,30 @@ require_once(dirname(__FILE__) . '/../vendor/autoload.php');
 $repo = new PayPlug\src\repositories\TranslationsRepository('');
 $translations = $repo->getTranslations();
 $missing_translations = [];
-$available_translations = [];
-$available_languages = ['fr','en','gb','it'];
+$available_languages = ['fr', 'en', 'gb', 'it'];
 $messages = [];
-
-foreach ($translations as $key => $trans) {
-    foreach ($available_languages as $lang) {
-        $key = str_replace("<{payplug}prestashop>", "", $key);
-        if (!$trans[$lang]) {
-            $missing_translations[$lang][$key] = $trans['default'];
-        } else {
-            $available_translations[$lang][$key]['default'] = $trans['default'];
-            $available_translations[$lang][$key]['lang'] = $trans[$lang];
-        }
-    }
-}
 
 // Open a file in write mode ('w')
 $fp = fopen(dirname(__FILE__) . '/translations.csv', 'w');
+$header = ['key', 'default'];
+$header = array_merge($header, $available_languages);
 
-// Loop through file pointer and a line
-foreach ($available_translations as $lang => $translations) {
-    foreach ($translations as $key => $trans) {
-        $trans = preg_replace("/\s+/", " ", $trans);
-        $line = [ $lang , $key, $trans['default'], $trans['lang']  ];
-        fputcsv($fp, $line);
+fputcsv($fp, $header);
+foreach ($translations as $key => $trans) {
+    $key = str_replace("<{payplug}prestashop>", "", $key);
+    $line = [$key, $trans['default']];
+    foreach ($available_languages as $lang) {
+        $line[] = $trans[$lang];
+
+        if (!$trans[$lang]) {
+            $missing_translations[$lang][$key] = $trans['default'];
+        }
     }
+    fputcsv($fp, $line);
 }
-
 fclose($fp);
 
+// Show missing translation
 if (!empty($missing_translations)) {
     $messages[] = '/!\ /!\ /!\ Some translations are missing /!\ /!\ /!\ ';
     foreach ($missing_translations as $lang => $translations) {
