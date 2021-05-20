9a2772bbcebf83ab62f108a492d93b75384c9528
hadrien de laforce <hdelaforce@payplug.com>
2021-05-20 09:14:56 +0200

    php-cs-fixer

--- ci/import-translations.php
+++ ci/import-translations.php
@@ -10,22 +10,19 @@ $_MODULE = array();
 
 foreach ($available_languages as $keyLang => $lang) {
     var_dump($lang);
-    $writeFile = fopen( dirname(__FILE__) . '/../translations/' . $lang . '.php', 'w');
-    fwrite( $writeFile , $headerContent);
+    $writeFile = fopen(dirname(__FILE__) . '/../translations/' . $lang . '.php', 'w');
+    fwrite($writeFile, $headerContent);
 
     $count = 0;
     $readFile = fopen('translations.csv', 'r');
     while (($line = fgetcsv($readFile)) !== false) {
         //$line is an array of the csv elements
-        if ($count && ($line[$keyLang + 2] != '') ) {
-
-            fwrite( $writeFile, '$_MODULE[\'<{payplug}prestashop>' . $line[0] . '\'] = \'' . addslashes($line[$keyLang + 2]) . '\';' . PHP_EOL);
-
+        if ($count && ($line[$keyLang + 2] != '')) {
+            fwrite($writeFile, '$_MODULE[\'<{payplug}prestashop>' . $line[0] . '\'] = \'' . addslashes($line[$keyLang + 2]) . '\';' . PHP_EOL);
         }
         $count++;
-      }
+    }
     $count = 0;
     fclose($readFile);
     fclose($writeFile);
-
 }
