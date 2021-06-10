93e1a8e45b2a280264191dda8d3f24179082c3e2
hadrien de laforce <hdelaforce@payplug.com>
2021-06-10 17:40:49 +0200

    update version module 3.2.0 to 3.2.1

--- classes/PayPlugClass.php
+++ classes/PayPlugClass.php
@@ -321,7 +321,7 @@ class PayPlugClass extends PaymentModule
         $this->need_instance = true;
         $this->ps_versions_compliancy = ['min' => '1.6', 'max' => '1.8'];
         $this->tab = 'payments_gateways';
-        $this->version = '3.2.0';
+        $this->version = '3.2.1';
         $this->oneyLogoUrl = '';
 
         $this->initializeAccessors();
