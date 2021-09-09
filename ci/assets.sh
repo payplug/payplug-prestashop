2434b26930f2cf059d60f58f2c3781c90410912f
Hadrien De Laforce <hdelaforce@payplug.com>
2021-09-09 12:36:46 +0000

    PRE-322 : compil assets via gitlab ci

--- ci/assets.sh
+++ ci/assets.sh
@@ -10,13 +10,21 @@ versionning_assets() {
       fi
       echo `basename ${file}`
       mv ${file} ${file%%.*}'-v'$version'.'$extension
-      # git grep -rl `basename ${file}` . ':!upgrade/*.php' ':!assets_revision.sh'
-      git grep -rl `basename ${file}` . ':!upgrade/*.php' ':!ci/assets_revision.sh' | xargs sed -i -e 's/'`basename ${file}`'/'`basename ${file%%.*}`'-v'$version'.'$extension'/g';
+      # git grep -rl `basename ${file}` . ':!upgrade/*.php' ':!assets.sh'
+      git grep -rl `basename ${file}` . ':!upgrade/*.php' ':!ci/assets.sh' | xargs sed -i -e 's/'`basename ${file}`'/'`basename ${file%%.*}`'-v'$version'.'$extension'/g';
   done
 }
 
 echo "Looking for tag in payplug.php..."
 tag=`grep '$this->version =' payplug.php | sed -n "s/.*= '//p" | sed -n "s/';//p"`
+
+echo "Starting files compilation"
+export lessFiles="admin admin_order front front_1_6"
+for file in $lessFiles
+  do
+    lessc ./views/css/${file}.less ./views/css/${file}.css
+  done
+echo "***** DONE *****"
 
 echo "Starting versioning JS file with" $tag "tag..."
 versionning_assets 'js' $tag
@@ -26,6 +34,14 @@ echo "Starting versioning CSS file with '" $tag "' tag..."
 versionning_assets 'css' $tag
 echo "***** DONE *****"
 
+echo "Moving file in build dir"
+mkdir ./views/build
+mkdir ./views/build/css
+mkdir ./views/build/js
+mv ./views/css/*.css ./views/build/css
+mv ./views/js/* ./views/build/js
+echo "***** DONE *****"
+
 echo " FINISH !!!"
-ls -l views/js/*.js
-ls -l views/css/*.css
+ls -l ./views/build/css/*.css
+ls -l ./views/build/js/*.js
