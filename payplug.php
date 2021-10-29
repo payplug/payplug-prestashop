20d15cea3f0fa514030080485fc740d159be0b68
Julien Hoarau <jhoarau@payplug.com>
2021-10-29 15:11:19 +0000

    PRE-513 : update module version to 3.5.0

--- payplug.php
+++ payplug.php
@@ -53,7 +53,7 @@ class Payplug extends PaymentModule
         $this->need_instance = true;
         $this->ps_versions_compliancy = ['min' => '1.6', 'max' => '1.8'];
         $this->tab = 'payments_gateways';
-        $this->version = '3.3.3';
+        $this->version = '3.5.0';
 
         parent::__construct();
 
