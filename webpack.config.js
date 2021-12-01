55bc0eb8a0cea635ed31c6ce20c467893fc6ed82
jhoarau <jhoarau@payplug.com>
2021-12-01 17:43:29 +0100

    add css minifyer

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const path = require('path');
 
 const lessFiles = ['front', 'admin', 'admin_order'];
 
 let entryFiles = {};
+entryFiles['payplug_front'] = ['./views/js/front.js','./views/js/embedded.js'];
+entryFiles['payplug16_front'] = ['./views/js/front_1_6.js','./views/js/embedded.js'];
+entryFiles['payplug_admin'] = ['./views/js/admin.js','./views/js/admin_order.js','./views/js/admin_order_popin.js'];
 
 lessFiles.map((file) => {
     entryFiles[file] = path.resolve(__dirname, 'views/css/' + file + '.less');
@@ -41,6 +45,21 @@ module.exports = {
                     },
                 ],
             },
+            {
+                test: /.s?css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"],
+            },
         ],
     },
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin(),
+        ],
+        minimize: true,
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        })
+    ]
 };
