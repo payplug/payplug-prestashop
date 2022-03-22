b4d932d59b28db48ebd01b18e1a284a6364c296d
jhoarau <jhoarau@payplug.com>
2022-03-22 18:19:12 +0100

    PRE-873 : add module key

--- pspaylater.php
+++ pspaylater.php
@@ -49,7 +49,7 @@ class PsPaylater extends PaymentModule
         $this->currencies_mode = 'checkbox';
         $this->description = $this->l('bnpl.construct.description');
         $this->displayName = 'Paylater';
-        $this->module_key = '1ee28a8fb5e555e274bd8c2e1c45e31a';
+        $this->module_key = '5c403c88b9f9097fca7fd50c4f25b86d';
         $this->need_instance = true;
         $this->ps_versions_compliancy = ['min' => '1.6', 'max' => '1.8'];
         $this->tab = 'payments_gateways';
