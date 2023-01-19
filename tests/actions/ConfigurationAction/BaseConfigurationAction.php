e912ab0e542dc9002bba0e25e3acda19c348bf13
jhoarau <jhoarau@payplug.com>
2023-01-19 12:26:41 +0100

    PRE-1704 : implement paylater bloc with vueJs lib

--- tests/actions/ConfigurationAction/BaseConfigurationAction.php
+++ tests/actions/ConfigurationAction/BaseConfigurationAction.php
@@ -12,13 +12,13 @@ class BaseConfigurationAction extends TestCase
     public $configuration;
     public $dependencies;
     public $logger;
+    public $module;
     public $oney;
     public $plugin;
+    public $validator;
 
     protected function setUp()
     {
-        $this->dependencies = MockHelper::createMockFactory('PayPlug\classes\DependenciesClass');
-
         $this->configuration = \Mockery::mock('Configuration');
         $this->configuration
             ->shouldReceive([
@@ -31,6 +31,18 @@ class BaseConfigurationAction extends TestCase
                 'addLog' => true,
             ]);
 
+        $module = \Mockery::mock('PrestashopModule');
+        $module
+            ->shouldReceive([
+                'enable' => true,
+            ]);
+
+        $this->module = \Mockery::mock('Module');
+        $this->module
+            ->shouldReceive([
+                'getInstanceByName' => $module,
+            ]);
+
         $this->oney = \Mockery::mock('Oney');
 
         $this->plugin = \Mockery::mock('Plugin');
@@ -39,13 +51,19 @@ class BaseConfigurationAction extends TestCase
                 'getLogger' => $this->logger,
                 'getConfiguration' => $this->configuration,
                 'getOney' => $this->oney,
+                'getModule' => $this->module,
             ]);
 
         $this->dependencies = MockHelper::createMockFactory('PayPlug\classes\DependenciesClass');
         $this->dependencies
             ->shouldReceive([
                 'getPlugin' => $this->plugin,
+                'getValidators' => [
+                    'payment' => \Mockery::mock(paymentValidator::class)->makePartial(),
+                ],
             ]);
+
+        $this->dependencies->name = 'payplug';
 
         $this->dependencies
             ->shouldReceive('getConfigurationKey')
