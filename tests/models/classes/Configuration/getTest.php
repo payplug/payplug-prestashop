98f239f649f775c226480037366b5395a244d8cf
Hadrien De Laforce <hdelaforce@payplug.com>
2023-02-03 18:44:53 +0100

    PRE-1721: Fix payment methods switch by adding mode

---
+++ tests/models/classes/Configuration/getTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace PayPlug\tests\models\classes\Configuration;
+
+/**
+ * @group unit
+ * @group classes
+ * @group configuration_classes
+ *
+ * @runTestsInSeparateProcesses
+ */
+class getTest extends BaseConfiguration
+{
+    public function invalidStringFormatDataProvider()
+    {
+        yield [42];
+        yield [['key' => 'value']];
+        yield [false];
+        yield [''];
+        yield [null];
+    }
+
+    /**
+     * @dataProvider invalidStringFormatDataProvider
+     *
+     * @param mixed $key
+     */
+    public function testWhenGivenKeyIsInvalidStringFormat($key)
+    {
+        $this->assertSame([], $this->classe->get($key));
+    }
+
+    public function testWhenGivenKeyDoesNotExistsInAllowedConfiguration()
+    {
+        $key = 'config_key';
+        $this->assertSame([], $this->classe->get($key));
+    }
+
+    public function testWhenConfigurationIsReturned()
+    {
+        $key = 'standard';
+        $this->assertSame(
+            [
+                'type' => 'integer',
+                'name' => 'STANDARD',
+                'defaultValue' => 1,
+                'setConf' => 1,
+            ],
+            $this->classe->get($key)
+        );
+    }
+}
---
+++ tests/models/classes/Configuration/getTypeTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace PayPlug\tests\models\classes\Configuration;
+
+/**
+ * @group unit
+ * @group classes
+ * @group configuration_classes
+ *
+ * @runTestsInSeparateProcesses
+ */
+class getTypeTest extends BaseConfiguration
+{
+    public function invalidStringFormatDataProvider()
+    {
+        yield [42];
+        yield [['key' => 'value']];
+        yield [false];
+        yield [''];
+        yield [null];
+    }
+
+    /**
+     * @dataProvider invalidStringFormatDataProvider
+     *
+     * @param mixed $key
+     */
+    public function testWhenGivenKeyIsInvalidStringFormat($key)
+    {
+        $this->assertSame('', $this->classe->getType($key));
+    }
+
+    public function testWhenGivenKeyDoesNotExistsInAllowedConfiguration()
+    {
+        $key = 'config_key';
+        $this->assertSame('', $this->classe->getType($key));
+    }
+
+    public function testWhenDefaultConfigurationIsReturned()
+    {
+        $key = 'standard';
+        $this->assertSame(
+            'integer',
+            $this->classe->getType($key)
+        );
+    }
+}
