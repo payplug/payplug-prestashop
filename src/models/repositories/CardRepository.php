a97665bfdc85c288a11e7ef109940e8ca273da52
Hadrien De Laforce <hdelaforce@payplug.com>
2024-08-20 16:47:28 +0200

    PRE-2575 fix(qa): Fix card saving

--- src/models/repositories/CardRepository.php
+++ src/models/repositories/CardRepository.php
@@ -42,12 +42,13 @@ class CardRepository extends EntityRepository
      * @param string $payment_id
      * @param int $company_id
      * @param false $is_sandbox
+     * @param mixed $id_card
      *
      * @return bool
      */
-    public function exists($payment_id = '', $company_id = 0, $is_sandbox = false)
+    public function exists($id_card = '', $company_id = 0, $is_sandbox = false)
     {
-        if (!is_string($payment_id) || !$payment_id) {
+        if (!is_string($id_card) || !$id_card) {
             return false;
         }
 
@@ -68,7 +69,7 @@ class CardRepository extends EntityRepository
             ->select()
             ->fields('id_card')
             ->from($this->getTableName($definition['table']))
-            ->where('id_card = "' . $this->escape($payment_id) . '"')
+            ->where('id_card = "' . $this->escape($id_card) . '"')
             ->where('id_company = ' . (int) $company_id)
             ->where('is_sandbox = ' . ((bool) $is_sandbox ? 1 : 0))
             ->build('unique_value');
@@ -163,77 +164,5 @@ class CardRepository extends EntityRepository
             ->engine($engine);
 
         return $this->build();
-    }
-
-    //TODO: to be deleted
-
-    /**
-     * @description Insert a new card in the database
-     *
-     * @param null $card
-     * @param int $customer_id
-     * @param int $company_id
-     * @param false $is_sandbox
-     *
-     * @return bool
-     */
-    public function set($card = null, $customer_id = 0, $company_id = 0, $is_sandbox = false)
-    {
-        if (!is_object($card) || !$card) {
-            return false;
-        }
-
-        if (!is_int($customer_id) || !$customer_id) {
-            return false;
-        }
-
-        if (!is_int($company_id) || !$company_id) {
-            return false;
-        }
-
-        if (!is_bool($is_sandbox)) {
-            return false;
-        }
-        if (!is_string($this->entity_name) || !$this->entity_name) {
-            return false;
-        }
-
-        $entity = $this->getEntityObject($this->entity_name);
-        if (!$entity) {
-            return false;
-        }
-        $definition = $entity->getDefinition();
-
-        $fields = [
-            'id',
-            'last4',
-            'exp_month',
-            'exp_year',
-            'brand',
-            'country',
-            'metadata',
-        ];
-        foreach ($fields as $field) {
-            if (!isset($card->{$field})) {
-                return false;
-            }
-        }
-
-        $result = $this
-            ->insert()
-            ->into($this->getTableName($definition['table']))
-            ->fields('id_customer')->values((int) $customer_id)
-            ->fields('id_company')->values((int) $company_id)
-            ->fields('is_sandbox')->values((bool) $is_sandbox ? 1 : 0)
-            ->fields('id_card')->values($this->escape($card->id))
-            ->fields('last4')->values($this->escape($card->last4))
-            ->fields('exp_month')->values($this->escape($card->exp_month))
-            ->fields('exp_year')->values($this->escape($card->exp_year))
-            ->fields('brand')->values($this->escape($card->brand))
-            ->fields('country')->values($this->escape($card->country))
-            ->fields('metadata')->values($this->escape(json_encode($card->metadata)))
-            ->build();
-
-        return (bool) $result;
     }
 }
