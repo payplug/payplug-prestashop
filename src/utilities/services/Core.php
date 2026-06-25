2008447859255c584ad93f92f97cf979d621a046
Hadrien de Laforce <hdelaforce@payplug.com>
2026-06-25 17:24:25 +0200

    PRE-3360 feat: Add refund mcp tool

--- src/utilities/services/Core.php
+++ src/utilities/services/Core.php
@@ -4,17 +4,10 @@ namespace PayPlug\src\utilities\services;
 
 use PayPlugPluginMcp\Actions\PaymentAction;
 use PayPlugPluginMcp\Models\Entities\PaymentInputDTO;
+use PayPlugPluginMcp\Models\Entities\RefundInputDTO;
 
 class Core
 {
-    /** @var PaymentAction */
-    private $payment_action;
-
-    public function __construct(PaymentAction $payment_action)
-    {
-        $this->payment_action = $payment_action;
-    }
-
     /**
      * @description create a payment resource using the core of the plugin and return the response
      *
@@ -23,7 +16,9 @@ class Core
     public function createCorePayment(PaymentInputDTO $payment_input)
     {
         try {
-            $payment_object = $this->payment_action->createAction($payment_input);
+            $payment_object = $this
+                ->getPaymentAction()
+                ->createAction($payment_input);
 
             return [
                 'result' => true,
@@ -37,5 +32,36 @@ class Core
                 'message' => $e->getMessage(),
             ];
         }
+    }
+
+    /**
+     * @description create a refund resource using the core of the plugin and return the response
+     *
+     * @return array
+     */
+    public function createCoreRefund(RefundInputDTO $refund_input)
+    {
+        try {
+            $payment_object = $this
+                ->getPaymentAction()
+                ->refundAction($refund_input);
+
+            return [
+                'result' => true,
+                'code' => 200,
+                'resource' => $payment_object->getResource(),
+            ];
+        } catch (\Exception $e) {
+            return [
+                'result' => false,
+                'code' => $e->getCode(),
+                'message' => $e->getMessage(),
+            ];
+        }
+    }
+
+    private function getPaymentAction(): PaymentAction
+    {
+        return new PaymentAction();
     }
 }
